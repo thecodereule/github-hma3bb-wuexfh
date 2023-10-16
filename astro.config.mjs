a0364c907cedf6f6e93917622b1c2aa947f625a9
thecodereule <igortadic.msc@gmail.com>
2023-10-16 17:52:18 +0200

    init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
+import solid from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  integrations: [solid()],
   integrations: [mdx(), sitemap(), tailwind()]
 });
